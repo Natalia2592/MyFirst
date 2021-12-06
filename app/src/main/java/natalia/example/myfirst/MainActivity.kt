ef60ebd0f9ff7658dcfa74264b16205c81aa5b41
Natalia <natal.kozyreva@gmail.com>
2021-12-07 02:15:15 +0300

    Styles and views

--- app/src/main/java/natalia/example/myfirst/MainActivity.kt
+++ app/src/main/java/natalia/example/myfirst/MainActivity.kt
@@ -1,40 +1,49 @@
 package natalia.example.myfirst
 
+import android.R.attr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-const val TAG = "Poem"
+import android.widget.Button
+import android.widget.TextView
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG,"Как бестолковы числа эти...")
+        val textView: TextView = findViewById(R.id.TextView)
+        val upButton: Button = findViewById(R.id.buttonUp)
+        val downButton: Button = findViewById(R.id.buttonDown)
+        val leftButton: Button = findViewById(R.id.buttonLeft)
+        val rightButton: Button = findViewById(R.id.buttonRight)
+
+        upButton.setOnClickListener {
+            textView.text = "Вверх"
+        }
+
+        textView.setOnClickListener {
+            textView.text = "Press any button"
+        }
+        downButton.setOnClickListener {
+            textView.text = "Вниз"
+        }
+
+        leftButton.setOnClickListener {
+            textView.text = "Влево"
+        }
+
+        rightButton.setOnClickListener {
+            textView.text = "Вправо"
+        }
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG,"Какой сумбур в календаре!")
-    }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG,"Теперь зима уж на дворе,")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG,"А мне вот довелось во всем ее расцвете,")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG,"В ее прелестнейшей поре,")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG,"Приветствовать Весну лишь в позднем ноябре.")
-    }
 }
